f3cfc10d798a4c2790e120110b5a06024acb72e1
kwonsye <kwonsye@naver.com>
2019-05-07 13:14:00 +0900

    create Like model and add foreign field of existed models

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from nomadgram.users import models as user_models
 
 # Create your models here.
 
 class TimeStampedModel(models.Model):
-    #base model for timestamp
+    
+    """ base model for timestamp """
+
     created_at = models.DateTimeField(auto_now_add=True) # 인스턴스가 생성될때마다 자동으로 채워짐
     updated_at = models.DateTimeField(auto_now=True) # 인스턴스가 업데이트 될때마다 업데이트됨
 
@@ -11,11 +14,25 @@ class TimeStampedModel(models.Model):
         abstract = True # 다른 class의 base abstract class 로 사용될 것임 https://docs.djangoproject.com/en/1.11/topics/db/models/#abstract-base-classes
 
 class Image(TimeStampedModel):
-    # 사진 model
+    
+    """ 사진 model """
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True) # on_delete=models.CASCADE : creator모델이 지워지면 Image모델도 지워짐
     file = models.ImageField() # 이미지 파일
     location = models.CharField(max_length=140)
     caption = models.TextField()
 
 class Comment(TimeStampedModel):
-    # 댓글 model
+    
+    """ 댓글 model """
+    
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
     message = models.TextField()
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+
+class Like(TimeStampedModel):
+
+    """ 좋아요 model """
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE,null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
