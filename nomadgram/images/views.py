8980ce26f73bb2cf4e536d438456ae3d01a4df98
kwonsye <kwonsye@naver.com>
2019-05-15 15:12:00 +0900

    add image view : ListAllImageView

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import models, serializers
 
-# Create your views here.
+class ListAllImages(APIView):
+
+    def get(self, request, format=None):
+        """for get request"""
+        all_images = models.Image.objects.all() # get all Image data from db
+        serializer = serializers.ImageSerializer(all_images, many=True) # for serialize images object
+        
+        return Response(data = serializer.data)
