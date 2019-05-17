1acb0a9bda8f798a2b729ae94a09f9429b8e900d
kwonsye <kwonsye@naver.com>
2019-05-17 18:30:00 +0900

    add list all comments view and link to url : /images/comments

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from nomadgram.images.views import (
     list_all_images_view,
+    list_all_comments_view,
 )
 
 app_name = "images"
 urlpatterns = [
     path("all/", view=list_all_images_view, name="list_all_images"), #/images/all -> list all images
+    path("comments/", view=list_all_comments_view, name="list_all_comments"), #/images/comments -> list all comments
 ]
--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -7,8 +7,19 @@ class ListAllImages(APIView):
     def get(self, request, format=None):
         """for get request"""
         all_images = models.Image.objects.all() # get all Image data from db
-        serializer = serializers.ImageSerializer(all_images, many=True) # for serialize images object
+        serializer = serializers.ImageSerializer(all_images, many=True) # for serialize image objects
         
-        return Response(data = serializer.data)
+        return Response(data=serializer.data)
 
 list_all_images_view = ListAllImages.as_view()
+
+class ListAllComments(APIView):
+
+    def get(self, request, format=None):
+        
+        all_comments = models.Comment.objects.all() # get all Comment data from db
+        serializer = serializers.CommentSerializer(all_comments, many=True) # for serialize comment objects
+
+        return Response(data=serializer.data)
+
+list_all_comments_view = ListAllComments.as_view()
