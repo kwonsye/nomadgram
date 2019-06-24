c67ee2e1d2da55ba61e24e2c4e92d0d8e49d2af6
kwonsye <kwonsye@naver.com>
2019-06-24 13:45:12 +0900

    create Notification model

---
+++ nomadgram/notifications/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from nomadgram.users import models as user_models
+from nomadgram.images import models as image_models
+
+class Notification(image_models.TimeStampedModel):
+
+    TYPE_CHOICES = (
+        ('like','Like'),
+        ('comment','Comment'),
+        ('follow','Follow'),
+    )
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name="creator_notification") # User에 대한 외래키가 두개이므로 related_name 필수
+    to = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name="to_notification")
+    notification_type = models.CharField(max_length=40, choices=TYPE_CHOICES)
+    image = models.ForeignKey(image_models.Image,on_delete=models.CASCADE, blank=True) # like와 comment 가 달린 image
