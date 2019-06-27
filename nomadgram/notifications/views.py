ff0ecbf7433444a34ce89e3042f71bb081d3d0fa
kwonsye <kwonsye@naver.com>
2019-06-27 13:55:43 +0900

    create notification view and link to /notifications

--- nomadgram/notifications/views.py
+++ nomadgram/notifications/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . import models, serializers
 
-# Create your views here.
+class Notifications(APIView):
+    
+    def get(self, request, format=None):
+        """request.user의 알람을 보여준다. """
+        
+        found_notifications = models.Notification.objects.filter(to=request.user)
+        serializer = serializers.NotificationSerializer(found_notifications, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+notifications_view = Notifications.as_view()
