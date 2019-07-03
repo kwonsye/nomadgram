a3419e1d3cc493e2499d5e56bfb493777b24cd3e
kwonsye <kwonsye@naver.com>
2019-07-03 20:25:32 +0900

    create notification function

--- nomadgram/notifications/views.py
+++ nomadgram/notifications/views.py
@@ -7,10 +7,22 @@ class Notifications(APIView):
     
     def get(self, request, format=None):
         """request.user의 알람을 보여준다. """
-        
+
         found_notifications = models.Notification.objects.filter(to=request.user)
         serializer = serializers.NotificationSerializer(found_notifications, many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 notifications_view = Notifications.as_view()
+
+def create_notification(creator, to, notification_type, image=None, comment=None): # image와 comment는 안들어올 수도 있기 때문에 디폴트 None
+    
+    new_notification = models.Notification.objects.create(
+        creator=creator,
+        to=to,
+        notification_type=notification_type,
+        image=image,
+        comment=comment
+        )
+
+    new_notification.save()
