c74b527d0c9d162d88b136bfee0ba84434289daa
kwonsye <kwonsye@naver.com>
2019-08-07 16:33:54 +0900

    modify database settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,7 +13,16 @@ ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["kwons.co"])
 #DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 #DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 #DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
-
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : env('RDS_DB_NAME'),
+        'USER' : env('RDS_DB_USERNAME'),
+        'PASSWORD' : env('RDS_DB_PASSWORD'),
+        'HOST' : env('RDS_DB_HOSTNAME'),
+        'PORT' : env('RDS_DB_PORT'),
+    }
+}
 # CACHES
 # ------------------------------------------------------------------------------
 CACHES = {
@@ -32,27 +41,27 @@ CACHES = {
 # SECURITY
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+#SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
-SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+#SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
-SESSION_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
-CSRF_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
-SECURE_HSTS_SECONDS = 60
+#SECURE_HSTS_SECONDS = 60
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
-SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
-    "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True
-)
+#SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
+#    "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True
+#)
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-preload
-SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+#SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
 # https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
-SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
-    "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True
-)
+#SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
+#    "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True
+#)
 
 # STORAGES
 # ------------------------------------------------------------------------------
